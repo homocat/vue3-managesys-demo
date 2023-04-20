9cd63d7197965ea55f4fea98302ff7ee749b92a6
ErefIect <luodan_0464@qq.com>
2023-04-20 19:40:47 +0800

    router

---
+++ src/main.js
@@ -0,0 +1,12 @@
+import { createApp } from "vue";
+import "./style.css";
+import App from "./App.vue";
+import ElementPlus from "element-plus";
+import "element-plus/dist/index.css";
+import "virtual:windi.css";
+import router from "./router/index";
+
+const app = createApp(App);
+app.use(ElementPlus);
+app.use(router);
+app.mount("#app");
---
+++ src/router/index.js
@@ -0,0 +1,29 @@
+import { createRouter, createWebHashHistory } from "vue-router";
+
+import Index from "~/pages/Index.vue";
+import About from "~/pages/About.vue";
+import NotFound from "~/pages/404.vue";
+import Login from "~/pages/Login.vue";
+
+const routes = [
+  {
+    path: "/",
+    component: Index,
+  },
+  {
+    path: "/login",
+    component: Login,
+  },
+  {
+    path: "/about",
+    component: About,
+  },
+  { path: "/:pathMatch(.*)*", name: "NotFound", component: NotFound },
+];
+
+const router = createRouter({
+  history: createWebHashHistory(),
+  routes,
+});
+
+export default router;
